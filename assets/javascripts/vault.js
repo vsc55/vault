26a0c20275a6b76759802e9e25da98dc7c2e3c07
Javier Pastor <jpastor@cerebelum.net>
2019-03-09 11:44:15 +0100

    remove Mixed spaces and tabs

--- assets/javascripts/vault.js
+++ assets/javascripts/vault.js
@@ -1,19 +1,18 @@
 $(function () {
-	
 	function copyToClipboard( elementId ) {
-	    var $temp = $( "<input>" ).val( $( "#" + elementId ).text() );
-	    $( "body" ).append($temp);
-	    $temp.select();
-	    document.execCommand("copy");
-	    $temp.remove();
+		var $temp = $( "<input>" ).val( $( "#" + elementId ).text() );
+		$( "body" ).append($temp);
+		$temp.select();
+		document.execCommand("copy");
+		$temp.remove();
 	}
 
 	$("a.copy-key").click(function () {
-	  copyToClipboard($(this).data('clipboard-target'));
+		copyToClipboard($(this).data('clipboard-target'));
 	});
 
 	// toggle label display between actual passwords and *****
-    $('td.key-password-column label').click(function () {
-      $(this).parents("td:first").find('label').toggle();
-    });
+	$('td.key-password-column label').click(function () {
+		$(this).parents("td:first").find('label').toggle();
+	});
 });
